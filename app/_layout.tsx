9fe4055e881c03d59ce8ba9f3889ac21ca1d9ce3
anjulaUmayanga <anjulaumayanga047@gmail.com>
2025-08-17 13:07:20 +0530

    Structure app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,54 @@
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
+import { SplashScreen, Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useCallback, useEffect } from 'react';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+  const [fontsLoaded] = useFonts({
+        'Poppins-Thin': require('../assets/fonts/Poppins-Thin.ttf'),
+        'Poppins-ThinItalic': require('../assets/fonts/Poppins-ThinItalic.ttf'),
+        'Poppins-ExtraLight': require('../assets/fonts/Poppins-ExtraLight.ttf'),
+        'Poppins-ExtraLightItalic': require('../assets/fonts/Poppins-ExtraLightItalic.ttf'),
+        'Poppins-Light': require('../assets/fonts/Poppins-Light.ttf'),
+        'Poppins-LightItalic': require('../assets/fonts/Poppins-LightItalic.ttf'),
+        'Poppins-Regular': require('../assets/fonts/Poppins-Regular.ttf'),
+        'Poppins-Italic': require('../assets/fonts/Poppins-Italic.ttf'),
+        'Poppins-Medium': require('../assets/fonts/Poppins-Medium.ttf'),
+        'Poppins-MediumItalic': require('../assets/fonts/Poppins-MediumItalic.ttf'),
+        'Poppins-SemiBold': require('../assets/fonts/Poppins-SemiBold.ttf'),
+        'Poppins-SemiBoldItalic': require('../assets/fonts/Poppins-SemiBoldItalic.ttf'),
+        'Poppins-Bold': require('../assets/fonts/Poppins-Bold.ttf'),
+        'Poppins-BoldItalic': require('../assets/fonts/Poppins-BoldItalic.ttf'),
+        'Poppins-ExtraBold': require('../assets/fonts/Poppins-ExtraBold.ttf'),
+        'Poppins-ExtraBoldItalic': require('../assets/fonts/Poppins-ExtraBoldItalic.ttf'),
+        'Poppins-Black': require('../assets/fonts/Poppins-Black.ttf'),
+        'Poppins-BlackItalic': require('../assets/fonts/Poppins-BlackItalic.ttf'),
+    });
 
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
+  const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
 
+     useEffect(() => {
+        if (fontsLoaded) {
+            (Text as unknown as { defaultProps: { style?: object } }).defaultProps = {
+                style: {fontFamily: 'Poppins-Regular'},
+            };
+            SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded]);
+    useEffect(() => {
+        onLayoutRootView();
+    }, [fontsLoaded, onLayoutRootView]);
+
+   
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
